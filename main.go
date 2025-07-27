0a1919274960809e3d149d7a7f553c849ad8ff41
Thiago Aires da Silva <makarrao@gmail.com>
2025-07-27 17:27:10 -0300

    Add Pizza struct and initialize pizza slice in main function

--- main.go
+++ main.go
@@ -2,7 +2,22 @@ package main
 
 import "fmt"
 
+type Pizza struct {
+	ID    int
+	nome  string
+	preco float64
+}
+
 func main() {
-	var nomePizzaria string = "Pizza a goGO"
-	fmt.Println(nomePizzaria)
+	// toscana, marguerita, atum com queijo
+	// nomePizzaria := "Pizza a goGO"
+	// insta, phone := "@pizza_go", 11911111111
+	// fmt.Println(nomePizzaria, insta, phone)
+
+	var pizzas = []Pizza{
+		{ID: 1, nome: "Toscana", preco: 12.94},
+		{ID: 2, nome: "Toscana", preco: 65.12},
+		{ID: 3, nome: "Toscana", preco: 123.94},
+	}
+	fmt.Print(pizzas)
 }
